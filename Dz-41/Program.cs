321d97f65cce9b312e43ccfe5c1430d5e431ec77
Anatolii <zavorinanatolii@mail.ru>
2022-08-27 19:38:21 +0800

    Dz-41

---
+++ Dz-41/Program.cs
@@ -0,0 +1,24 @@
+﻿        
+// Пользователь вводит M чисел. Посчитайте количество чисел больше 0.
+
+
+int num(int M)
+{
+    int sum =0;
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine("Ввведите число:  ");
+        int numer =  int.Parse(Console.ReadLine());
+        if (numer > 0)
+        {
+            sum ++;
+        }
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Ввведите количество чисел:  ");
+int M =  int.Parse(Console.ReadLine());
+int myArray = num(M);   
+Console.WriteLine($"Количество положительных чисел {myArray}");
